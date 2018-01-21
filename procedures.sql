8fc45af443d0973d7e02b96486f7958ac9bfaa04
Miłosz <milosz.mandowski@owlsdepartment.com>
2018-01-21 21:00:21 +0100

    dodanie 1 procedury

--- procedures.sql
+++ procedures.sql
@@ -17,7 +17,7 @@ BEGIN
 			@Discount
 		)
 END
-
+GO
 
 
 CREATE PROCEDURE P_AddWorkshop
@@ -42,10 +42,11 @@ BEGIN
 			@WorkshopEnd
 		)
 END 
-
+GO
 
 
 EXEC P_AddWorkshop @ConferenceID = 4, @ConferenceDay = 1, @WorkshopName = 'WorkshopProcedureTest', @Places = 35, @WorkshopFee = 20, @WorkshopStart = '9:00', @WorkshopEnd = '10:30';
+GO
 
 CREATE PROCEDURE P_AddPriceToConferencePriceList
 	@ConferenceID int,
@@ -61,9 +62,8 @@ BEGIN
 			@PriceDate
 		)
 END
+GO
 
-SELECT *
-FROM PriceList
 
 CREATE PROCEDURE P_DeletePriceFromConferencePriceList
 	@PriceID int
@@ -81,8 +81,45 @@ BEGIN
 	DELETE PriceList
 		WHERE PriceID = @PriceID 
 END
+GO
 
-SELECT * 
-FROM PriceList
-EXEC P_AddPriceToConferencePriceList @ConferenceID = 4, @PriceValue = 200, @PriceDate = '2001-04-13';
-EXEC P_DeletePriceFromConferencePriceList @PriceID = 16; 
+-- EXEC P_AddPriceToConferencePriceList @ConferenceID = 4, @PriceValue = 200, @PriceDate = '2001-04-13';
+-- EXEC P_DeletePriceFromConferencePriceList @PriceID = 16;
+
+-- anulowanie rezerwacji na dzien konferencji
+CREATE PROCEDURE P_CancelDayReservation
+	@DayReservationID int
+AS
+BEGIN
+	IF NOT EXISTS
+		(
+			SELECT * FROM DaysReservations WHERE DayReservationID = @DayReservationID
+		)
+	BEGIN
+		RAISERROR ('Nie istnieje taki dzień', -1, -1)
+	END
+
+	IF (
+		SELECT IsCancelled FROM DaysReservations WHERE DayReservationID = @DayReservationID
+		) = 1
+	BEGIN
+		RAISERROR ('Rezerwacja jest już anulowana', -1, -1)
+	END
+	
+	BEGIN
+		UPDATE DaysReservations
+		SET IsCancelled = 1
+		WHERE DayReservationID = @DayReservationID
+
+		UPDATE ParticipantReservations
+		SET IsCancelled = 1
+		WHERE DayReservationID = @DayReservationID
+		
+		UPDATE WorkshopsReservations
+		SET IsCancelled = 1
+		WHERE DayReservationID = @DayReservationID
+
+		-- update participantworkshops
+	END
+END	
+GO
